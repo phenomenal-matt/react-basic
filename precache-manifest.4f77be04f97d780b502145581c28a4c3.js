5d2211db4da93e77401d32de501656b340f44531
Matthew Akhaze <akhmatthew@gmail.com>
2018-12-04 17:13:17 +0100

    Updates

---
+++ precache-manifest.4f77be04f97d780b502145581c28a4c3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cb94cafc2fc67d3a981c",
+    "url": "/react-basic/static/js/runtime~main.cb94cafc.js"
+  },
+  {
+    "revision": "910807557e331dc2a3c7",
+    "url": "/react-basic/static/js/main.91080755.chunk.js"
+  },
+  {
+    "revision": "65ce664db22ebc0891b6",
+    "url": "/react-basic/static/js/1.65ce664d.chunk.js"
+  },
+  {
+    "revision": "910807557e331dc2a3c7",
+    "url": "/react-basic/static/css/main.5f4fc82f.chunk.css"
+  },
+  {
+    "revision": "709a1c234b6832d16e8fd917deff3172",
+    "url": "/react-basic/index.html"
+  }
+];
